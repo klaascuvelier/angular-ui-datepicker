05f13afc04413bc0e6d5c4a0711b34569f4c5749
Klaas Cuvelier <klaas@showpad.com>
2013-08-24 15:29:42 +0200

    getting started

---
+++ src/ui-datepicker.js
@@ -0,0 +1,12 @@
+angular.module('ui-components', [])
+    .directive('datePicker', [ '$scope',
+        function ($scope) {
+
+            var definition = {
+
+            };
+
+            return definition;
+
+        }
+    ]);
